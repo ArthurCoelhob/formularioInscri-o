73f34672bb5bf4ce41fa44974d2a7af3155ed5d6
Arthur <arthur.silva@teknisa.com>
2022-03-08 23:03:09 -0300

    adicionando validação de campos no form

--- script.php
+++ script.php
@@ -4,10 +4,29 @@ $categorias = [];
 $categorias[]  = 'infantil';
 $categorias[]  = 'adolescente';
 $categorias[]  = 'adulto';
-$categorias[]  = 'idoso';
 
 $nome = $_POST['nome'];
 $idade = $_POST['idade'];
+
+if(empty($nome)) {
+    echo 'O campo nome precisa ser preenchido';
+    return;
+}
+
+if(strlen($nome) < 3) {
+    echo 'O campo nome precisa conter mais de 3 caracteres';
+    return;
+}
+
+if(strlen($nome) > 40) {
+    echo 'O campo nome é muito extenso';
+    return;
+}
+
+if(!is_numeric($idade)) {
+    echo 'Informe um numero para o campo idade';
+    return;
+}
 
 if ($idade >= 6 && $idade <= 12) {
     echo "O nadador " .$nome. " compete na categoria " . $categorias[0];
