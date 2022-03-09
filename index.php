dd55eacf900889b2732abfb7b209f4660d9bf1d1
Arthur <arthur.silva@teknisa.com>
2022-03-08 21:39:37 -0300

    estrutura formulario com metodo POST

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulario de inscrição</title>
+</head>
+<body>
+    <p>FORMULARIO PARA INSCRIÇÃO DE COMPETIDORES</p>
+
+<form action="script.php"
+    method="POST">
+    <p>Seu nome: <input type="text" name="nome"/></p>
+    <p>Seu idade: <input type="text" name="idade"/></p>
+    <p><input type="submit"/></p>
+</form>
+</body>
+
+
+</html>
